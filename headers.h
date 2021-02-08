a66b743aa5e8ae43acfab9dc8cb3481f7bb07893
Pwnisher <ethancruize@gmail.com>
2021-02-09 01:53:42 +0530

    Add files via upload

---
+++ headers.h
@@ -0,0 +1,10 @@
+#include<Windows.h>
+#include<stdio.h>
+#include <objbase.h>
+#include<combaseapi.h>
+#include <exdisp.h>
+#include <mshtml.h>
+#include <Urlhist.h>
+#include <winternl.h>
+#include <intrin.h>
+#include <Rpc.h>
